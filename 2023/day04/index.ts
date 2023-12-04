2825fb9f8b4117eb6df853df1389cd8006373d62
DucktorScience <46750327+DucktorScience@users.noreply.github.com>
2023-12-04 17:52:45 +0000

    2023 day 4 part 2

--- 2023/day04/index.ts
+++ 2023/day04/index.ts
@@ -3,11 +3,12 @@ import { INPUT } from "./input";
 
 import _intersection from 'lodash/intersection';
 import _sum from 'lodash/sum';
+import _times from 'lodash/times';
 
 const ORIGINAL_LINES = INPUT.split('\n');
 
-const getScoreForLine = (line: string) => {
-  const [cardNo, numbers] = line.split(': ');
+const getTotalMatchingNumbersForLine = (line: string) => {
+  const [_cardId, numbers] = line.split(': ');
   const trimmedNumbers = removeExtraSpacing(numbers);
   const [winningSource, actualSource] = trimmedNumbers.split(' | ');
 
@@ -15,8 +16,13 @@ const getScoreForLine = (line: string) => {
   const actualNumbersArray = (actualSource.split(' ').map(Number));
 
   const intersectionArray = _intersection(winningNumbersArray, actualNumbersArray);
-  const totalMatchingNumbers = intersectionArray.length;
+  return intersectionArray.length;
+}
 
+// index represents the scratchcard id (minus one) and value represents how many numbers are winning
+const TotalWinningNumbers = ORIGINAL_LINES.map(getTotalMatchingNumbersForLine);
+
+const getScoreFromTotalWinningNumbers = (totalMatchingNumbers: number) => {
   if (totalMatchingNumbers === 0) {
     return 0;
   }
@@ -27,8 +33,23 @@ const getScoreForLine = (line: string) => {
   return Math.pow(2, totalMatchingNumbers - 1);
 }
 
-const totalPoints = ORIGINAL_LINES.map(line => getScoreForLine(line));
+const totalPoints = TotalWinningNumbers.map(getScoreFromTotalWinningNumbers);
+
+// Part 2
+
+// Start with a copy of each unique scratchcard id
+const scratchCardIdsCache = _times(ORIGINAL_LINES.length);
+for (const scratchCardId of scratchCardIdsCache) {
+  const totalWinningNumbersForThisScratchCard = TotalWinningNumbers[scratchCardId];
+  if (totalWinningNumbersForThisScratchCard) {
+    for (let i = 0; i < totalWinningNumbersForThisScratchCard; ++i) {
+      const bonusCardIndex = scratchCardId + 1 + i;
+      scratchCardIdsCache.push(bonusCardIndex);
+    }
+  }
+}
 
 console.log({
-  part1: _sum(totalPoints),
+  part1: _sum(totalPoints), // 26346
+  part2: scratchCardIdsCache.length,  // 8467762
 });
