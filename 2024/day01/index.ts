c435b9c56dc7c0173c452ee8d3d14cc90916d4d2
DucktorScience <46750327+DucktorScience@users.noreply.github.com>
2024-12-04 19:51:11 +0000

    solve 2024 day 01 part 2

--- 2024/day01/index.ts
+++ 2024/day01/index.ts
@@ -5,19 +5,21 @@ const lines = INPUT.split('\n');
 const left: number[] = [];
 const right: number[] = [];
 
+const timesAppearingInTheRightList = new Map<number, number>();
+
 lines.forEach(line => {
   const parts = line.split(' ');
   left.push(Number(parts[0]));
-  right.push(Number(parts.at(-1)));
+
+  const rightNumber = Number(parts.at(-1));
+  right.push(rightNumber);
+
+  const currentLookup = timesAppearingInTheRightList.get(rightNumber) ?? 0;
+  timesAppearingInTheRightList.set(rightNumber, 1 + currentLookup)
 });
 
 left.sort(ascendingComparator);
 right.sort(ascendingComparator);
-
-console.log({
-  left,
-  right
-})
 
 const distances = left.map((a, index) => {
   const b = right[index];
@@ -25,7 +27,11 @@ const distances = left.map((a, index) => {
   return Math.abs(a - b);
 });
 
-console.log({ distances })
-
 const summed = sumArray(distances);
-console.log({ summed });
+console.log({ part1: summed });
+
+const similarities = left.map((a) => {
+  return a * (timesAppearingInTheRightList.get(a) ?? 0);
+});
+
+console.log({ part2: sumArray(similarities) })
