996c37075e6ab18e5a3de6107558fa51d5fc2450
DucktorScience <46750327+DucktorScience@users.noreply.github.com>
2024-12-09 15:07:15 +0000

    solve 2024 day 02 part 2

--- 2024/day02/index.ts
+++ 2024/day02/index.ts
@@ -42,5 +42,23 @@ const isSafe = (row: Array<number>) => {
 }
 
 const safeRows = rows.filter(isSafe)
-
 console.log(`Part1 = ${safeRows.length}`);
+
+const isSafeRowWithTolerance = (row: Array<number>) => {
+  const isGenerallySafe = isSafe(row);
+  if (isGenerallySafe) {
+    return true;
+  }
+
+  for (let i = 0; i < row.length; ++i) {
+    const rowWithoutPositionI = row.toSpliced(i, 1);
+    if (isSafe(rowWithoutPositionI)) {
+      return true;
+    }
+  }
+
+  return false
+}
+
+const tolerableRows = rows.filter(isSafeRowWithTolerance)
+console.log(`Part2 = ${tolerableRows.length}`);
