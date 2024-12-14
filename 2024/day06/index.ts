12b30276e9c7791310036cddfac5b4057439dbfa
DucktorScience <46750327+DucktorScience@users.noreply.github.com>
2024-12-14 16:01:29 +0000

    solve 2024 day 06 part 2

--- 2024/day06/index.ts
+++ 2024/day06/index.ts
@@ -2,38 +2,100 @@ import assert from "assert";
 import { CharacterGrid } from "utilities/grid";
 import { INPUT } from "./input"
 import { PointSet } from "utilities/space/PointSet";
-import { Direction, nudgeDirection, Point, turnTo } from "utilities/space";
+import { Direction, nudgeDirection, Point, pointsEqual, turnTo } from "utilities/space";
+import { KeyedSet } from "utilities/collections";
 
 const lines = INPUT.split('\n');
 
 const OUT_OF_BOUNDS = '@'
 const OBSTACLE = '#';
-const grid = new CharacterGrid(lines, OUT_OF_BOUNDS);
+const masterGrid = new CharacterGrid(lines, OUT_OF_BOUNDS);
 
-const startPoint = grid.find('^');
+const startPoint = masterGrid.find('^');
 assert(startPoint);
 
-const getTotalVisitedPoints = () => {
+type DirectionAndPosition = {
+  direction: Direction;
+  position: Point;
+}
+
+const getDirectionAndPositionKey = (value: DirectionAndPosition) => `(${value.direction})[${value.position.x}, ${value.position.y}]`;
+class DirectionAndPositionSet extends KeyedSet<DirectionAndPosition> {
+  constructor() {
+    super(getDirectionAndPositionKey);
+  }
+}
+
+const isLooping = (grid: CharacterGrid) => {
   let direction: Direction = 'N';
   let position: Point = startPoint;
-  const visited = new PointSet();
-  visited.add(startPoint);
+
+  const visited = new DirectionAndPositionSet();
+  visited.add({ direction, position })
 
   while (true) {
     const nextPosition = nudgeDirection(position, direction);
     const nextValue = grid.getValue(nextPosition.x, nextPosition.y);
 
     if (nextValue === OUT_OF_BOUNDS) {
-      return visited.size;
+      return false;
+    }
+
+    if (nextValue === OBSTACLE) {
+      direction = turnTo(direction, 'R');
+    }
+    else {
+      position = nextPosition;
+    }
+
+
+    if (visited.has({ direction, position })) {
+      return true;
+    }
+
+    visited.add({ direction, position });
+  }
+}
+
+const solve = () => {
+  let direction: Direction = 'N';
+  let position: Point = startPoint;
+  const visited = new PointSet();
+  visited.add(startPoint);
+
+  const loopingObstacleLocations = new PointSet();
+
+  while (true) {
+    const nextPosition = nudgeDirection(position, direction);
+    const nextValue = masterGrid.getValue(nextPosition.x, nextPosition.y);
+
+    if (nextValue === OUT_OF_BOUNDS) {
+      return {
+        totalVisitedPoints: visited.size,
+        totalObstacleCandidates: loopingObstacleLocations.size,
+      }
     }
 
     if (nextValue === OBSTACLE) {
       direction = turnTo(direction, 'R');
     } else {
+
+      if (!pointsEqual(nextPosition, startPoint)) {
+        // What if that was an obstacle!
+        const theoreticalGrid = masterGrid.clone();
+        theoreticalGrid.setValue(nextPosition.x, nextPosition.y, OBSTACLE);
+
+        if (!loopingObstacleLocations.has(nextPosition) && isLooping(theoreticalGrid)) {
+          loopingObstacleLocations.add(nextPosition);
+        }
+      }
+
       position = nextPosition;
       visited.add(nextPosition);
     }
   }
 }
 
-console.log({ part1: getTotalVisitedPoints() });
+const { totalVisitedPoints: part1, totalObstacleCandidates: part2 } = solve();
+
+console.log({ part1, part2 });
